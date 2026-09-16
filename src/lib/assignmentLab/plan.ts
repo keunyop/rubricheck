c4440948c7024b5334123f1f84d3c203488d9f01
Kevin <kylee1112@hotmail.com>
2026-09-15 22:37:06 -0700

    lab 기능 삭제

--- src/lib/assignmentLab/plan.ts
+++
@@ -1,6 +0,0 @@
-﻿import { getAccountEntitlementByEmail, isActiveProAccountEntitlement } from "../accountEntitlements";
-
-export async function labIsPro(email: string): Promise<boolean> {
-  return isActiveProAccountEntitlement(await getAccountEntitlementByEmail(email));
-}
-
